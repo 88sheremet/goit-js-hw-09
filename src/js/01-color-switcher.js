75304228d9b2854d7db90928a5a8dcdb69683f25
Oleksandr Sheremet <88sheremet@gmail.com>
2022-11-21 20:12:05 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,29 @@
 
-
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
+
+ const startButton = document.querySelector('[data-start]'); 
+console.log(startButton);
+ const stopButton = document.querySelector('[data-stop]');
+ console.log(stopButton);
+ const body = document.querySelector('body');
+
+ let timerId = null;
+
+ stopButton.disabled = true;
+ 
+ startButton.addEventListener('click', () =>{
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+  stopButton.disabled = false;
+ });
+
+ stopButton.addEventListener("click", () => {
+  clearInterval(timerId);
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  
+});
